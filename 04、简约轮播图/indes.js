8b25f0247d2e94bd4bd8ccaf8fcc59a72f9eaf44
Droliz <96186700+Droliz@users.noreply.github.com>
2023-01-29 12:49:35 +0800

    自动轮播

--- 04、简约轮播图/indes.js
+++ 04、简约轮播图/indes.js
@@ -1,9 +1,41 @@
-document.querySelectorAll('.s_button')[1].onclick = () => {
+// 手动切换
+// document.querySelectorAll('.s_button')[1].onclick = () => {
+//     let lists = document.querySelectorAll('.item');
+//     document.querySelector('#slide').appendChild(lists[0]);
+// }
+
+// document.querySelectorAll('.s_button')[0].onclick = () => {
+//     let lists = document.querySelectorAll('.item');
+//     document.querySelector('#slide').prepend(lists[lists.length - 1]);
+// }
+
+
+// 控制图片切换
+let _direction = true;  // 控制切换方向
+let _move = () => {
     let lists = document.querySelectorAll('.item');
-    document.querySelector('#slide').appendChild(lists[0]);
+    if (_direction) {
+        document.querySelector('#slide').appendChild(lists[0]);
+    } else {
+        document.querySelector('#slide').prepend(lists[lists.length - 1]);
+    }
+}
+
+let timer = setInterval(_move, 2500);
+// 鼠标移入暂停播放
+document.querySelector('.container').addEventListener('mouseover', () => {
+    clearInterval(timer);
+});
+document.querySelector('.container').addEventListener('mouseout', () => {
+    timer = setInterval(_move, 2500);
+});
+
+document.querySelectorAll('.s_button')[1].onclick = () => {
+    _direction = true;
+    _move();
 }
 
 document.querySelectorAll('.s_button')[0].onclick = () => {
-    let lists = document.querySelectorAll('.item');
-    document.querySelector('#slide').prepend(lists[lists.length - 1]);
+    _direction = false;
+    _move();
 }
